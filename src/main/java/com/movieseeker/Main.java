034d2e90611c9f1982faa916237efc72293bd352
Gabriel Massera <gmaszera@hotmail.com>
2024-07-29 23:35:05 -0300

    JDBC, ADD new movie, list all movies.

--- src/main/java/com/movieseeker/Main.java
+++ src/main/java/com/movieseeker/Main.java
@@ -1,7 +1,36 @@
 package com.movieseeker;
 
+import com.movieseeker.model.Filme;
+import com.movieseeker.repository.FilmeRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        FilmeRepository filmeRepository = new FilmeRepository();
 
+        // Adiciona um novo filme
+        Filme novoFilme = new Filme();
+        novoFilme.setNome("Monster Hunter");
+        novoFilme.setGenero("Sci-Fi");
+        novoFilme.setAno(2020);
+        novoFilme.setDiretor("Paul W. S. Anderson");
+        novoFilme.setAtor("Milla Jovovich");
+
+        try {
+            filmeRepository.addFilme(novoFilme);
+            System.out.println("Filme adicionado com sucesso!");
+
+            // Recupera e exibe todos os filmes
+            List<Filme> filmes = filmeRepository.getAllFilmes();
+            System.out.println("Filmes no banco de dados:");
+            for (Filme filme : filmes) {
+                System.out.println(filme.getNome() + " (" + filme.getAno() + ") - " + filme.getGenero() + " - Diretor: " + filme.getDiretor());
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao acessar o banco de dados: " + e.getMessage());
+        }
     }
 }
+
--- src/main/java/com/movieseeker/repository/FilmeRepository.java
+++ src/main/java/com/movieseeker/repository/FilmeRepository.java
@@ -1,4 +1,48 @@
 package com.movieseeker.repository;
 
+import com.movieseeker.model.Filme;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilmeRepository {
+
+    private Connection getConnection() throws SQLException {
+        //URL de conexão com banco
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/movieSeeker", "gmaszera", "130620");
+    }
+
+    public void addFilme(Filme filme) throws SQLException {
+        String query = "INSERT INTO movie (name, genre, year, director, actor) VALUES (?, ?, ?, ?, ?)";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, filme.getNome());
+            statement.setString(2, filme.getGenero());
+            statement.setInt(3, filme.getAno());
+            statement.setString(4, filme.getDiretor());
+            statement.setString(5, filme.getAtor());
+            statement.executeUpdate();
+        }
+
+    }
+
+    public List<Filme> getAllFilmes() throws SQLException {
+        List<Filme> filmes = new ArrayList<>();
+        String query = "SELECT * FROM movie";
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                Filme filme = new Filme();
+                filme.setNome(resultSet.getString("name"));
+                filme.setGenero(resultSet.getString("genre"));
+                filme.setAno(resultSet.getInt("year"));
+                filme.setDiretor(resultSet.getString("director"));
+                filme.setAtor(resultSet.getString("actor"));
+                filmes.add(filme);
+            }
+        }
+        return filmes;
+    }
 }
